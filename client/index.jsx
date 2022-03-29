f6c0a8207cace0754beb9aa99e6c3b523434902c
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-03-29 16:20:51 +0200

    39 min inn

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes, useHref } from "react-router-dom";
 
 function FrontPage() {
   return (
@@ -16,12 +16,38 @@ function FrontPage() {
   );
 }
 
+function Login() {
+  const [redirectUrl, setRedirectUrl] = useState();
+  useEffect(() => {
+
+
+      const authorization_endpoint =
+          "https://accounts.google.com/o/oauth2/v2/auth";
+      const parameters = {
+          response_type:"token",
+          client_id:"client",
+          scope:"email profile",
+          redirect_uri: window.location.origin +"/login/callback",
+      };
+      setRedirectUrl(
+      authorization_endpoint + "?" + new URLSearchParams(parameters)
+    );
+  }, []);
+  return (
+    <div>
+      <h1>Login</h1>
+        <a href={redirectUrl}>Login here</a>
+        <div>{redirectUrl}</div>
+    </div>
+  );
+}
+
 function Application() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<FrontPage />} />
-        <Route path={"/login"} element={<h1>Login</h1>} />
+        <Route path={"/login"} element={<Login />} />
         <Route path={"/login/callback"} element={<h1>Login callback</h1>} />
         <Route path={"/profile"} element={<h1>Profile</h1>} />
       </Routes>
