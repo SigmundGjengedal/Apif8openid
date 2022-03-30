82f65b74f67ae3e448b4ec1763a7de31a422e102
SigmundGjengedal <sigmund.gjengedal@gmail.com>
2022-03-30 16:38:38 +0200

    get access token and store ina cookie

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 import express from "express";
 import * as path from "path";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
-// serveren skal serve reactappen
+// endpoint LoginCallback
+app.post("/api/login",(req,res) =>{
+  const {access_token} = req.body;
+  res.cookie("access_token",access_token, {signed:true});
+  res.sendStatus(200);
+})
+
+// endpoint reactappen
 app.use(express.static("../client/dist"));
 
+//endpoint for react browserrouter  * GET ! /api
 app.use((req, res, next) => {
   if (req.method === "GET" && !req.path.startsWith("/api")) {
     res.sendFile(path.resolve("../client/dist/index.html"));
